feff92c25921e15daa73d5d094feb01b8b4e2e53
Alex <alexcc17@gmail.com>
2019-11-18 22:42:40 -0500

    Clean up

--- xAnimation/ViewController.swift
+++ xAnimation/ViewController.swift
@@ -17,31 +17,16 @@ class ViewController: UIViewController {
     // MARK: - Outlets
     
     @IBOutlet weak var x3Lbl: UILabel!
+    var messageLabel = UILabel()
     
     // MARK: - Actions
     
     @IBAction func startBtnPressed(_ sender: UIButton) {
-        isHiddenNow.toggle()
-        x3Lbl.isHidden = isHiddenNow
-        
-        print("Running spring animation from button")
-        x3Lbl.center = self.view.center
-
-        self.x3Lbl.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
-        UIView.animate(withDuration: 3.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
-            self.x3Lbl.transform = .identity
-        }, completion: nil)
+        runAnimation()
     }
     
     @objc func springBtnPressed(){
-        print("Running spring animation")
-        x3Lbl.center = self.view.center
-
-        self.x3Lbl.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
-        UIView.animate(withDuration: 3.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
-            self.x3Lbl.transform = .identity
-        }, completion: nil)
-        
+        runAnimation()
     }
     
     // MARK: - VC Lifecycle
@@ -49,13 +34,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        x3Lbl.isHidden = isHiddenNow
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(springBtnPressed))
-        tap.numberOfTouchesRequired = 3
-        x3Lbl.addGestureRecognizer(tap)
+        setupUI()
     }
 
+    // MARK: - Animation
+    
+    private func runAnimation(){
+        isHiddenNow.toggle()
+        messageLabel.isHidden = isHiddenNow
+        
+        print("Running spring animation from button")
+        messageLabel.center = self.view.center
+
+        self.messageLabel.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        UIView.animate(withDuration: 3.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: [], animations: {
+            self.messageLabel.transform = .identity
+        }, completion: nil)
+    }
+    // MARK: - Setup UI
+    
+    private func setupUI(){
+        x3Lbl.isHidden = true
+        
+        // Message Label
+        messageLabel.text = "x4"
+        messageLabel.font = UIFont(name: "Marker Felt", size: 80)
+        messageLabel.isHidden = isHiddenNow
+        messageLabel.sizeToFit()
+        messageLabel.textColor = #colorLiteral(red: 0.2784313725, green: 0.8862745098, blue: 0.8078431373, alpha: 1)
+        messageLabel.center = self.view.center
+        
+        // Gesture Recognizer
+        let tap = UITapGestureRecognizer(target: self, action: #selector(springBtnPressed))
+        tap.numberOfTouchesRequired = 3
+        messageLabel.addGestureRecognizer(tap)
+     
+        view.addSubview(messageLabel)
+    }
 
 }
 
